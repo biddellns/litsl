c5798ed1a5ee7f1b2b9d755b9dda5b362e818807
Nicholas Biddell <nicbiddell@gmail.com>
2016-11-02 21:26:04 -0400

    First unit test passes. Checks if URL routes to the right view

--- players/tests.py
+++ players/tests.py
@@ -2,10 +2,11 @@ from django.test import TestCase
 
 from django.core.urlresolvers import resolve
 
-from players.views import PlayerList
+import players
+#from players.views import PlayerList
 # Create your tests here.
 
 class PlayerListPageTest(TestCase):
     def test_root_slash_players_resolves_to_player_list_view(self):
-        found = resolve('^$')
-        self.assertEqual(found.func, PlayerList.as_view())
+        found = resolve('/players/')
+        self.assertEqual(found.view_name, 'players.views.PlayerList')
