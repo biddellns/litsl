16ee4e8cedd35d9420364958c1a24aef22ef13fe
Nicholas Biddell <nicbiddell@gmail.com>
2016-12-01 21:14:54 -0500

    Season front page template uses player model method to display number of matches played.

--- players/models.py
+++ players/models.py
@@ -31,5 +31,12 @@ class Player(models.Model):
     ladder_games_played = models.IntegerField(blank = True, null = True)
     team = models.CharField(max_length = 30)
 
+    def get_total_games(self):
+        wins = 0
+        losses = 0
+        total_games = self.matchups_player1.count() + self.matchups_player2.count()
+        
+        return total_games
+
     def __str__(self):
         return '{}#{}'.format(self.bnet_name, self.bnet_id)
