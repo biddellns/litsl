4584737b93d9050a4ef95876d64777cc0fbd8d27
Nicholas Biddell <nicbiddell@gmail.com>
2016-12-19 21:28:28 -0500

    Shows schedule for current round of current season. Add link to navbar.

--- season/urls.py
+++ season/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import url
 
-from .views import SeasonDetail
+from .views import SeasonDetail, ScheduleList
 urlpatterns = [
         url(r'^$', SeasonDetail.as_view(), 
             name='season_detail_default'),
         url(r'^(?:(?P<pk>[0-9]+))?/$', SeasonDetail.as_view(),
             name='season_detail'),
+        url(r'^(?P<season>[0-9]+)/schedule/$', ScheduleList.as_view(),
+            name="season_schedule"),
+        url(r'^schedule/$', ScheduleList.as_view(),
+            name="season_schedule_default"),
 #        url(r'^(?P<pk>[0-9]+)/$', PlayerDetail.as_view(), name='player_detail'),            
         ]
--- season/views.py
+++ season/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import DetailView, FormView
+from django.views.generic import DetailView, ListView, FormView
 
-from .models import Season
+from .models import Season, GroupRound
 
 # Create your views here.
 class SeasonDetail(DetailView):
@@ -25,7 +25,7 @@ class SeasonDetail(DetailView):
         if pk is not None:
             queryset = queryset.filter(pk=pk)
         else:
-            pk = queryset.last() # 
+            pk = queryset.last() # Most recent season.
         # Next, try looking up by slug.
         if slug is not None and (pk is None or self.query_pk_and_slug):
             slug_field = self.get_slug_field()
@@ -49,5 +49,53 @@ class SeasonDetail(DetailView):
         group_round = obj.group_rounds.last()
         groups = group_round.groups.all().prefetch_related('players')
         context['groups'] = groups
+
+        return context
+
+class ScheduleList(DetailView):
+    model = GroupRound
+    context_object_name = 'group_round'
+    template_name = 'seasons/schedule.html'
+    
+    def get_object(self, queryset=None):
+        """
+        Returns the object the view is displaying.
+        By default this requires `self.queryset` and a `pk` or `slug` argument
+        in the URLconf, but subclasses can override this to return any object.
+        """
+        # Use a custom queryset if provided; this is required for subclasses
+        # like DateDetailView
+        if queryset is None:
+            queryset = self.get_queryset()
+        # Next, try looking up by primary key.
+        pk = self.kwargs.get(self.pk_url_kwarg)
+        slug = self.kwargs.get(self.slug_url_kwarg)
+        if pk is not None:
+            queryset = queryset.filter(pk=pk)
+        else:
+            pk = queryset.last() # Most recent group round.
+        # Next, try looking up by slug.
+        if slug is not None and (pk is None or self.query_pk_and_slug):
+            slug_field = self.get_slug_field()
+            queryset = queryset.filter(**{slug_field: slug})    
+        # If none of those are defined, it's an error.
+        if pk is None and slug is None:
+            raise AttributeError("Generic detail view %s must be called with "
+                                "either an object pk or a slug."
+                                % self.__class__.__name__)
+        try:
+            # Get the single item from the filtered queryset
+            obj = queryset.get()
+        except queryset.model.DoesNotExist:
+            raise Http404(_("No %(verbose_name)s found matching the query") %
+                         {'verbose_name': queryset.model._meta.verbose_name})
+        return obj
+
+    def get_context_data(self, **kwargs):
+        context = super(ScheduleList, self).get_context_data(**kwargs)
+        group_round = self.get_object()
+
+        groups = group_round.groups.all()
+        context["groups"] = groups
 
         return context
