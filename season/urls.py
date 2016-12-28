a18061eb0cbfda6e5901b8d51237103704b6c166
Nicholas Biddell <nicbiddell@gmail.com>
2016-12-27 23:45:28 -0500

    Refactor season.urls to nest routes under season number with an optional capture group.

--- season/urls.py
+++ season/urls.py
@@ -1,16 +1,13 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 
 from .views import SeasonDetail, ScheduleList, RulesView
 urlpatterns = [
-        url(r'^$', SeasonDetail.as_view(), 
-            name='season_detail_default'),
-        url(r'^(?:(?P<pk>[0-9]+))?/$', SeasonDetail.as_view(),
-            name='season_detail'),
-        url(r'^(?P<season>[0-9]+)/schedule/$', ScheduleList.as_view(),
-            name="season_schedule"),
-        url(r'^schedule/$', ScheduleList.as_view(),
-            name="season_schedule_default"),
-        url(r'^rules/$', RulesView.as_view(),
-            name="rules"),
-#        url(r'^(?P<pk>[0-9]+)/$', PlayerDetail.as_view(), name='player_detail'),            
+        url(r'^(?:(?P<pk>[0-9]+)/)?', include([
+            url(r'^$', SeasonDetail.as_view(), 
+                name="season_detail"),
+            url(r'schedule/$', ScheduleList.as_view(),
+                name="season_schedule"),
+            url(r'^rules/$', RulesView.as_view(),
+                name="rules"),
+            ])),
         ]
