b9dabda61ee50c184631c8e91773b9d8efe55374
Nicholas Biddell <nicbiddell@gmail.com>
2017-01-22 11:53:29 -0500

    Changed location of static files

--- litsl/settings/staging.py
+++ litsl/settings/staging.py
@@ -26,9 +26,9 @@ DATABASES = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, "static"),
-        'static/',
-        ]
+STATIC_URL = '../static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '..','static/')
+#STATICFILES_DIRS = [
+ #       os.path.join(BASE_DIR, "static"),
+ #       'static/',
+ #       ]
