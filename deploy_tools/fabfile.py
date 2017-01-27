43d97fc2a4219283719b0f1973fc6f6e1392b7a6
Nicholas Biddell <nicbiddell@gmail.com>
2017-01-26 20:46:24 -0500

    Enabled _update_static_files()

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -1,43 +1,48 @@
 from fabric.contrib.files import append, exists, sed
 from fabric.api import env, local, run
 
-
 REPO_URL = 'https://github.com/biddellns/litsl.git'
 VENV_NAME = 'litsl-staging'
 
+
 def deploy():
-		site_folder = '/home/{0}/sites/litsl_com_fab'.format(env.user)  
-		source_folder = site_folder + '/source'
-		_create_directory_structure(site_folder)
-		_get_latest_source(source_folder)
-		_update_settings(source_folder, env.domain) 
-		_update_virtualenv(env.user, VENV_NAME, source_folder)
-	        #_update_static_files(source_folder, env.user, VENV_NAME)
-		# _update_database(source_folder)
+    site_folder = '/home/{0}/sites/litsl_com_fab'.format(env.user)
+    source_folder = site_folder + '/source'
+    _create_directory_structure(site_folder)
+    _get_latest_source(source_folder)
+    _update_settings(source_folder, env.domain)
+    _update_virtualenv(env.user, VENV_NAME, source_folder)
+    _update_static_files(source_folder, env.user, VENV_NAME)
+
+
+# _update_database(source_folder)
 
 def _create_directory_structure(site_folder):
     for subfolder in ('static', 'source'):
         run('mkdir -p {0}/{1}'.format(site_folder, subfolder))
 
+
 def _get_latest_source(source_folder):
     # Has the git repo already been cloned?
     if exists(source_folder + '/.git'):
         # Fetch latest updates
-        run ('cd {0} && git fetch'.format(source_folder))
+        run('cd {0} && git fetch'.format(source_folder))
     else:
         # Clone the reop
         run('git clone {0} {1}'.format(REPO_URL, source_folder))
-			
+
     latest_commit = local("git log -n 1 --format=%H", capture=True)
     run('cd {0} && git reset --hard {1}'.format(source_folder, latest_commit))
+
 
 def _update_settings(source_folder, domain):
     # For staging
     settings_path = source_folder + '/litsl/settings/staging.py'
     sed(settings_path,
-            'ALLOWED_HOSTS = .+$',
-            'ALLOWED_HOSTS = ["{0}", "www.{0}"]'.format(domain)
+        'ALLOWED_HOSTS = .+$',
+        'ALLOWED_HOSTS = ["{0}", "www.{0}"]'.format(domain)
         )
+
 
 def _update_virtualenv(env_user, venv_name, source_folder):
     virtualenv_folder = '/home/{0}/.virtualenvs/{1}'.format(env_user, venv_name)
@@ -50,6 +55,8 @@ def _update_virtualenv(env_user, venv_name, source_folder):
     # Install requirements from requirements.txt
     run('{0}/bin/pip3 install -r {1}/requirements.txt'.format(virtualenv_folder, source_folder))
 
+
 def _update_static_files(source_folder, env_user, venv_name):
-    run('source .virtualenvs/{2}/bin/activate && cd {0} && /home/{1}/.virtualenvs/{2}/bin/python3 manage.py collectstatic --noinput'
-                                                        .format(source_folder, env_user, venv_name))
+    run(
+        'workon {2} && cd {0} && /home/{1}/.virtualenvs/{2}/bin/python3 manage.py collectstatic --noinput'
+        .format(source_folder, env_user, venv_name))
