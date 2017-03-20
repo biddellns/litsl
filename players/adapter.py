dfa4dc407b0ee09444f3ed6c50ea2856520fd476
Nicholas Biddell <nicbiddell@gmail.com>
2017-03-20 00:29:20 -0400

    Create and link Player object to user after all_auth saves the new User.

--- players/adapter.py
+++ players/adapter.py
@@ -1,24 +1,44 @@
+import requests, json
+
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 
 from .models import Player
 
 class MyPlayerAdapter(DefaultSocialAccountAdapter):
-    def pre_social_login(self, request, sociallogin):
-        """
-        Invoked just after a user successfully authenticates via a
-        social login provider but before the login is actually processed
-        (and before the pre_social_login signal is emitted).
 
-        We're trying to solve for two use cases:
-        - social account already exists: Keep going through flow.
-        - social account is new: Create new player model and link it.
-        """
+    """
+    After an intial user is saved (validated), populate a Player object
+    and link it to the newly created user.
+    """
+    def save_user(self, request, sociallogin, form = None):
+        user = super().save_user(request, sociallogin, form)
 
-        # Ignore existing social accounts. They should already be linked to
-        # a player model.
-        # 
-        # Else, create a new player object.
-        if sociallogin.is_existing:
-            return
-        else:
-            player = Player.objects.create()
+        # Build URL to access user data
+
+        # Base URL
+        url = "https://us.api.battle.net/sc2/profile/user?"
+
+        # User will have only one blizzard account and therefore only one token
+        socialtoken = user.socialaccount_set.first().socialtoken_set.first() 
+        
+        params = dict(
+                access_token = socialtoken,
+                )
+
+        # Make call for user data
+        user_json_data = requests.post(url=url, params=params).json()
+
+        # Grab the first character we see
+        sc2_profile = user_json_data['characters'][0]
+
+        sc2_name = sc2_profile['name']
+        team = sc2_profile['clanName'] or " "
+        
+        # Grab first letter for db
+        ladder_games_played = sc2_profile['season']['totalGamesThisSeason'][:1]
+        
+        league = sc2_profile['career']['highest1v1Rank'] # Get the highest 1v1 league ever.
+
+        player = Player.objects.create(user=user, sc2_name=sc2_name, team=team)
+        player.save()
+
